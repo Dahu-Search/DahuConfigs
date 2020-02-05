c6188f634f429ed27b543d6e09352d8b00e860a6
Vince McNamara <vince@dahu.co.uk>
2020-02-05 17:21:29 +0000

    removed index.html, added config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const configs = {
+    googleclientid : "104594742807-r9tns6theuch8pkj6hne2bpl85rnijvo.apps.googleusercontent.com",
+    debug : "false",
+    appname : "OPC Search",
+    signin : "Google",
+};
