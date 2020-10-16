d9d456db907ce34d4d0b9210ea7cf87a21aabac3
Vince McNamara <vince@dahu.co.uk>
2020-10-16 15:37:02 +0100

    updated config for v1.1.0

--- v4/docRoot/DahuSearch/config.js
+++
@@ -1,9 +0,0 @@
-const configs = {
-    googleclientid : "360146944373-fc0ueql4fm9kqfu75g5re86h3sdp68tk.apps.googleusercontent.com",
-    debug : "false",
-    appname : "Dahu Search",
-	basename : "/DahuSearch",
-    signin : "Google",
-	suggest: "queries",
-    PDFViewer:true
-};
