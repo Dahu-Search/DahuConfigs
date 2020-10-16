f78a40aa1b469fc1ac08c4f24d3ef972f6eec351
Vince McNamara <vince@dahu.co.uk>
2020-10-16 15:59:58 +0100

    base config, docRoot, adminroot

---
+++ v4/adminRoot/config.js
@@ -0,0 +1,17 @@
+const configs = {
+    debug : "false",
+    basename : "admin",
+    serverurl : "/admin",
+    appname : "Dahu Admin",
+    signin : "Google",
+    refreshInterval:"2000",
+    google :{
+        googleclientid : "360146944373-fc0ueql4fm9kqfu75g5re86h3sdp68tk.apps.googleusercontent.com",
+        endUserEmulation:false
+    },
+    azure :{
+        microsoftclientid : "4a770322-470e-42e5-90b8-5dac6952d8b3",
+        tenantUrl : "https://login.microsoftonline.com/24209931-90be-4929-8a86-9a180f5611ad",
+    }
+
+};
---
+++ v4/docRoot/DahuSearch/config.js
@@ -0,0 +1,28 @@
+const configs = {
+    debug : "false",
+    appname : "Dahu Search",
+    signin : "Google",
+	suggest: "queries",
+    PDFViewer:true,
+    features:[
+        "SavedConstraints",
+        "HomePageHeaderSearchOptions",
+        "HomePageHeader",
+	"HomePageFilters",
+        "HomePageHeaderSavedSearches-X",
+        "ResultsPageSearchOptions",
+        "ResultsPageHeaderSavedSearches",
+        "ResultShowParentLink",
+        "ResultShowSourceIcon"
+    ]
+    ,
+    google :{
+        googleclientid : "360146944373-fc0ueql4fm9kqfu75g5re86h3sdp68tk.apps.googleusercontent.com",
+        endUserEmulation:false
+    },
+    azure :{
+        microsoftclientid : "4a770322-470e-42e5-90b8-5dac6952d8b3",
+        tenantUrl : "https://login.microsoftonline.com/24209931-90be-4929-8a86-9a180f5611ad",
+    }
+
+};
